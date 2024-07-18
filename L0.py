41a36c332e4b69cf4abeab43374300a2a307f2be
Marissha <balabobalaba71@gmail.com>
2024-07-18 20:07:30 +0300

    Add files via upload

---
+++ L0.py
@@ -0,0 +1,12 @@
+    #1st program
+print(9**0.5*5)
+
+    # 2st program
+print(9.99>9.98 and 1000 !=1000.1)
+
+    # 3st program
+print(((1234//10)%100)+((5678//10)%100))
+
+    # 4st program
+print((int(13.42 // 1) == int(42.13 % 42.0*100)) or (int(13.42 % 13.0 * 100.1 )) == int(42.13//1))
+
